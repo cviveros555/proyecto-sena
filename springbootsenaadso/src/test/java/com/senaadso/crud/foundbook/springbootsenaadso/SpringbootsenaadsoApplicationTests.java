bcb793af2b707544c157e85cd595bd711628282b
cviveros555 <cavilo11@gmail.com>
2024-07-31 22:48:30 -0500

    inicial-proyecto
    proyecto inicial

---
+++ springbootsenaadso/src/test/java/com/senaadso/crud/foundbook/springbootsenaadso/SpringbootsenaadsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senaadso.crud.foundbook.springbootsenaadso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsenaadsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
